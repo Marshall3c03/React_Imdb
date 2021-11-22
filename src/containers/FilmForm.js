472d5317bbcc9e78561f5c49fb23b284429f5559
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-22 20:13:31 +0000

    form inputs now have textchanging handlers applied

--- src/containers/FilmForm.js
+++ src/containers/FilmForm.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const FilmForm = () => {
+
+    let [title, setTitle] = useState("");
+    let [url, setUrl] = useState("");
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value)
+    }
+
     return(
         <form>
-            <input type="text" placeholder="Film Title"></input>
-            <input type="text" placeholder="IMDB Url"></input>
-            <input type="submit" value="Add Film"></input>
+            <input 
+            type="text" 
+            placeholder="Film Title" 
+            value={title}
+            onChange={handleTitleChange}/>
+
+            <input 
+            type="text" 
+            placeholder="IMDB Url" 
+            value={url}
+            onChange={handleUrlChange}/>
+
+            <input type="submit" value="Add Film"/>
         </form>
     )
 }
