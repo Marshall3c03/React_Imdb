d7198641e996c363642c32a419b0c11b07b1c3b5
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-22 19:55:05 +0000

    Managed to get view more button to redirect to the imdb page

--- src/components/UpcomingFilmsButton.js
+++ src/components/UpcomingFilmsButton.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const UpcomingFilmsButton = () => {
+
+    const loadImdbWebpage = (event) => {
+        event.preventDefault();
+        window.location.href='https://www.imdb.com/calendar/?region=gb';
+        }
+
+
     return (
-        <>
-        <button>View more upcoming releases >></button>
-        </>
+        <button type="button" onClick={loadImdbWebpage}>
+            View more upcoming releases >>
+        </button>
     )
 }
 
