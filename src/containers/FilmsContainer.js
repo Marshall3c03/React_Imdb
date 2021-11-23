d3d3776d789aabd5c65e422f86714d7805236b29
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-23 09:17:13 +0000

    set initialfilms state in a variable, this looks tidier

--- src/containers/FilmsContainer.js
+++ src/containers/FilmsContainer.js
@@ -5,34 +5,34 @@ import FilmForm from './FilmForm';
 
 const FilmsContainer = () => {
 
-    let [films, setFilms] = useState(
-        [
-            {
-              id: 1,
-              title: "Spider-Man: Into the Spider-Verse",
-              url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
-            },
-            {
-              id: 2,
-              title: "Life Itself",
-              url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
-            },
-            {
-              id: 3,
-              title: "Mary Queen of Scots",
-              url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
-            },
-            {
-              id: 4,
-              title: "The Lego Movie 2: The Second Part", url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
-            },
-            {
-              id: 5,
-              title: "Captain Marvel",
-              url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
-            }
-          ]          
-    )
+  const initialFilms = [
+    {
+      id: 1,
+      title: "Spider-Man: Into the Spider-Verse",
+      url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+    },
+    {
+      id: 2,
+      title: "Life Itself",
+      url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+    },
+    {
+      id: 3,
+      title: "Mary Queen of Scots",
+      url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+    },
+    {
+      id: 4,
+      title: "The Lego Movie 2: The Second Part", url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+    },
+    {
+      id: 5,
+      title: "Captain Marvel",
+      url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+    }
+  ]
+
+    let [films, setFilms] = useState(initialFilms)
 
     const addFilm = (newFilm) => {
         newFilm.id = Date.now();
